6231139fc7d757331c8b0979e7f490eeec597402
Yatoom <jmapvhoof@gmail.com>
2016-01-26 11:46:24 +0100

    Added basic structure.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <!doctype html>
+<meta charset="utf-8">
+<title>Budgr</title>
 <link rel="stylesheet" href="dist/css/main.css">
 <script src="dist/js/vendor.js"></script>
 <script src="dist/js/all.js"></script>
+
+<div class="row">
+    <div class="small-12 columns">
+        <h1>Budgr</h1>
+    </div>
+</div>
