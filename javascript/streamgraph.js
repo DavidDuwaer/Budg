6533d8db33f7ea0470815c4a05dec13af5523b34
D.F. Duwaer <davidduwaer@gmail.com>
2016-01-28 13:34:09 +0100

    Added main.js, moved some stuff from streamGraph.js to main.js

--- javascript/streamgraph.js
+++ javascript/streamgraph.js
@@ -6,11 +6,7 @@ var layers0, layers1, area;
 
 function drawStreamGraph()
 {
-    var n = 20, // number of layers
-        m = 200, // number of samples per layer
-        stack = d3.layout.stack().offset("wiggle");
-    //layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); })),
-    //layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
+    var stack = d3.layout.stack().offset("wiggle");
     layers0 = stack(multipleTimeSeries);
     layers1 = stack(multipleTimeSeries);
 
@@ -65,7 +61,7 @@ function drawStreamGraph()
     /*
      * Add in-graph labels
      */
-    var labels = canvas.selectAll("text")
+    canvas.selectAll("text")
         .data(layers0)
         .enter()
         .append("text");
@@ -161,51 +157,11 @@ function transition() {
  */
 //    var color = d3.scale.linear()
 //            .range(["#c30", "#ea8"]);
-var color = d3.scale.category20();
 
 /*
  * Make data arrays
  */
-var yearValues = ["2013", "2014", "2015", "2016"];
-var ministryValues = [
-    "A: Infrastructuurfonds",
-    "B: Gemeentefonds",
-    "C: Provinciefonds",
-    "F: Diergezondheidsfonds",
-    "H: BES-fonds",
-    "I: De Koning",
-    "IIa: De Staten Generaal",
-    "IIb: Overige Hoge Colleges van Staat",
-    "III: Algemene Zaken",
-    "IV: Koninkrijksrelaties",
-    "IXa: Nationale Schuld",
-    "IXb: Financiën",
-    //"Rijk",
-    "J: Deltafonds",
-    "V: Buitenlandse Zaken",
-    "VI: Veiligheid en Justitie",
-    "VII: Binnenlandse Zaken en Koninkrijksrelaties",
-    "VIII: Onderwijs: Cultuur en Wetenschap",
-    "X: Defensie",
-    "XII: Infrastructuur en Milieu",
-    "XIII: Economische Zaken",
-    "XV: Sociale Zaken en Werkgelegenheid",
-    "XVI: Volksgezondheid: Welzijn en Sport",
-    "XVII: Buitenlandse Handel & Ontwikkelingssamenwerking",
-    "XVIII: Wonen en Rijksdienst"
-];
-var sideValues = ["O", "U", "V"];
-var sideSigns = [-1, 1, 1];
-console.log(yearValues[0]);
-var yearsScale = d3.scale.linear()
-    .domain(yearValues)
-    .range(d3.range(0, yearValues.length - 1, 1));
-var ministriesScale = d3.scale.ordinal()
-    .domain(ministryValues)
-    .range(d3.range(0, ministryValues.length - 1, 1));
-var sidesScale = d3.scale.ordinal()
-    .domain(sideValues)
-    .range(sideSigns);
+
 
 var multipleTimeSeries = [];
 for (var i = 0; i < ministryValues.length; i++)
